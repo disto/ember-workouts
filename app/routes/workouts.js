bb0efcba67bf4f078614dbd7d8d9e0e10d13b402
disto <dstokkel@12move.nl>
2015-01-28 17:06:24 +0100

    routes/workouts.js changed json.get back to store.find

--- app/routes/workouts.js
+++ app/routes/workouts.js
@@ -3,5 +3,10 @@ import Ember from 'ember';
 export default Ember.Route.extend({
     model: function() {
         return this.store.find('workouts');
+//        var url = 'http://wo.thingit.nl/workouts';
+//        return Ember.$.getJSON(url).then(function(workouts) {
+//            return workouts;
+//        });
     }
+
 });
